e4bee15ef718c4631b70d9634ac3623bd1950455
Steve Smith <steve@kentsmiths.com>
2021-05-17 14:15:13 -0400

    add memostack

---
+++ WithoutGenerics/MemoStack.cs
@@ -0,0 +1,33 @@
+﻿namespace WithoutGenerics
+{
+    public class MemoStack
+    {
+        private MemoStackItem _top = null;
+
+        public void Push(Memo memo)
+        {
+            var item = new MemoStackItem(_top, memo);
+            _top = item;
+        }
+
+        public Memo Pop()
+        {
+            if (_top is null) return null;
+            var val = _top.Value;
+            _top = _top.NextItem;
+            return val;
+        }
+
+        private class MemoStackItem
+        {
+            public MemoStackItem NextItem { get; private set; }
+            public Memo Value { get; private set; }
+
+            public MemoStackItem(MemoStackItem nextItem, Memo value)
+            {
+                NextItem = nextItem;
+                Value = value;
+            }
+        }
+    }
+}
