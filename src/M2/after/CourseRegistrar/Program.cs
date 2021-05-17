2f1e9b97ac1d368838f81d5fa745383a290acb71
Steve Smith <steve@kentsmiths.com>
2021-05-17 16:00:10 -0400

    M2 initial version with string waitlist

---
+++ src/M2/after/CourseRegistrar/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+
+namespace CourseRegistrar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Course Registration System");
+            Console.WriteLine("--------------------------");
+
+            // add student email to waitlist
+            var waitlist = new Waitlist();
+
+            Console.WriteLine("Adding students to waitlist...");
+
+            waitlist.Add("student1@test.test");
+            waitlist.Add("student2@test.test");
+            waitlist.Add("student3@test.test");
+
+            // course has open seats
+            bool courseIsFull = false;
+
+            // process waitlist
+            while (waitlist.Length > 0 && !courseIsFull)
+            {
+                string? studentEmail = (string?)waitlist.GetNextItem();
+                Console.WriteLine($"Emailing {studentEmail} to let them know about opening.");
+            }
+
+            Console.WriteLine("Finished.");
+            Console.ReadLine();
+        }
+    }
+
+    public class Waitlist
+    {
+        private Queue _queue = new Queue();
+        public int Length => _queue.Count;
+
+        public void Add(object item)
+        {
+            _queue.Enqueue(item);
+        }
+
+        public object? GetNextItem()
+        {
+            return _queue.Dequeue();
+        }
+    }
+
+    // later to gain type safety
+    public class EmailWaitlist
+    {
+        private Queue _queue = new Queue();
+        public int Length => _queue.Count;
+
+        public void Add(string item)
+        {
+            _queue.Enqueue(item);
+        }
+
+        public string? GetNextItem()
+        {
+            return (string?)_queue.Dequeue();
+        }
+    }
+}
---
+++ src/M2/before/README.md
@@ -0,0 +1,24 @@
+# Module 2
+
+Welcome!
+
+In this module we started from scratch, so there is nothing in this "before" folder.
+
+You'll find the completed code in the "after" folder.
+
+Look for the latest code on GitHub:
+
+https://github.com/ardalis/CSharpGenerics
+
+Thanks!
+
+Steve "ardalis" Smith
+
+Find or Connect with me:
+
+- [ardalis.com](https://ardalis.com)
+- [twitter](https://twitter.com/ardalis)
+- [github](https://github.com/ardalis)
+- [youtube](https://youtube.com/ardalis)
+- [mentoring and coaching](https://nimblepros.com)
+- [linkedin](https://www.linkedin.com/in/stevenandrewsmith/)
