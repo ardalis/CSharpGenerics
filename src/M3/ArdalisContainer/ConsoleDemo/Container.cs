aca1db5779310cc3ef4c38dfdd2564bf4ecd135f
Steve Smith <steve@kentsmiths.com>
2021-05-24 13:53:56 -0400

    Initial container with 4 tests - no generics yet

---
+++ src/M3/ArdalisContainer/ConsoleDemo/Container.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ArdalisContainer.ConsoleApp
+{
+    public class Container
+    {
+        Dictionary<Type, Type> _registry = new();
+
+        public void Register(Type type)
+        {
+            _registry.Add(type, type);
+        }
+
+        public object Resolve(Type type)
+        {
+            if(!_registry.ContainsKey(type))
+            {
+                throw new TypeResolutionException($"Request for type {type} received but not instance of that type has been registered.");
+            }
+
+
+            var typeToResolve = _registry[type];
+
+            var constructorInfo = typeToResolve.GetConstructors()
+                .First();
+            var parameters = constructorInfo.GetParameters()
+                .Select(p => Resolve(p.ParameterType))
+                .ToArray();
+
+            return Activator.CreateInstance(typeToResolve, parameters);
+        }
+
+        public void Register(Type requestedType, Type returnedType)
+        {
+            _registry.Add(requestedType, returnedType);
+
+        }
+    }
+
+    public class TypeResolutionException : Exception
+    {
+        public TypeResolutionException(string message) : base(message)
+        {
+        }
+    }
+}
---
+++ src/M3/ArdalisContainer/ConsoleDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ArdalisContainer.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
