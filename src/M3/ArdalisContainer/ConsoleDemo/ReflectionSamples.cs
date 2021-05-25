7986ce2aadb7cd5197b40cf29433072c28a8f946
Steve Smith <steve@kentsmiths.com>
2021-05-24 21:18:23 -0400

    adding reflection code

--- src/M3/ArdalisContainer/ConsoleDemo/ReflectionSamples.cs
+++ src/M3/ArdalisContainer/ConsoleDemo/ReflectionSamples.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Reflection;
 
 namespace ArdalisContainer.ConsoleApp
@@ -8,7 +9,8 @@ namespace ArdalisContainer.ConsoleApp
         public static void ListGenericMethods(Type type)
         {
             var methods = type.GetMethods(BindingFlags.Public |
-    BindingFlags.Instance);
+    BindingFlags.Instance | BindingFlags.Static)
+                .Where(method => method.DeclaringType.Name != "Object");
             Console.WriteLine($"Methods of type {type}:");
             Console.WriteLine("Name        |IsGeneric   |IsGenDefin  |ContainsGenParams");
             int colWidth = 12;
@@ -26,10 +28,15 @@ namespace ArdalisContainer.ConsoleApp
 
                 if (method.IsGenericMethod)
                 {
-                    // later
                     Console.WriteLine("Calling generic method:");
                     MethodInfo genericMethod = method.MakeGenericMethod(typeof(Customer));
-                    genericMethod.Invoke(Activator.CreateInstance(type), new[] { new Customer() });
+
+                    object instance = null; // use null for static methods
+                    if(!genericMethod.IsStatic)
+                    {
+                        instance = Activator.CreateInstance(type);
+                    }
+                    genericMethod.Invoke(instance, new[] { new Customer("Steve","Smith") });
                 }
             }
             Console.WriteLine();
@@ -37,9 +44,21 @@ namespace ArdalisContainer.ConsoleApp
 
         public static void Execute()
         {
-            var processorType = typeof(Processor<>);
-            Console.WriteLine($"Is Processor<> Generic? {processorType.IsGenericType}");
-            ListGenericMethods(processorType);
+            var openProcessorInterface = typeof(IProcessor<>);
+            Console.WriteLine($"Is IProcessor<> Generic? {openProcessorInterface.IsGenericType}");
+            ListGenericMethods(openProcessorInterface);
+
+            var closedProcessorInterface = typeof(IProcessor<Customer>);
+            Console.WriteLine($"Is IProcessor<Customer> Generic? {closedProcessorInterface.IsGenericType}");
+            ListGenericMethods(closedProcessorInterface);
+
+            var openProcessorClass = typeof(Processor<>);
+            Console.WriteLine($"Is Processor<> Generic? {openProcessorClass.IsGenericType}");
+            ListGenericMethods(openProcessorClass);
+
+            var closedProcessorClass = typeof(Processor<Customer>);
+            Console.WriteLine($"Is Processor<Customer> Generic? {closedProcessorClass.IsGenericType}");
+            ListGenericMethods(closedProcessorClass);
 
             var cpType = typeof(CustomerProcessor);
             Console.WriteLine($"Is CustomerProcessor Generic? {cpType.IsGenericType}");
@@ -49,7 +68,7 @@ namespace ArdalisContainer.ConsoleApp
             Console.ReadLine();
         }
 
-        public interface IProcess<T>
+        public interface IProcessor<T>
         {
             void Process(T input);
         }
@@ -59,16 +78,17 @@ namespace ArdalisContainer.ConsoleApp
             void Log<T>(T input);
         }
 
-        public class Customer { }
-        public class Processor<T> : IProcess<T>
+        public record Customer(string firstName, string lastName);
+        public class Processor<T> : IProcessor<T>
         {
             public void Process(T input)
             {
                 Console.WriteLine($"Generic Processor of T, processing {input}");
             }
+
         }
 
-        public class CustomerProcessor : IProcess<Customer>, ILogger
+        public class CustomerProcessor : IProcessor<Customer>, ILogger
         {
             public void Log<T>(T input)
             {
@@ -78,6 +98,13 @@ namespace ArdalisContainer.ConsoleApp
             public void Process(Customer input)
             {
                 Console.WriteLine($"Process Customer: {input}");
+            }
+
+            // trying to invoke this on a generic class is more difficult
+            // in general avoid having class<A> containing static method<B>
+            public static void ExpediteProcess<TExpedited>(TExpedited input)
+            {
+                Console.WriteLine($">>> EXPEDITE! {input}");
             }
         }
     }
