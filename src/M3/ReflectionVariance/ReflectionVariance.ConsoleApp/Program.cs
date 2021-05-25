c4f1452e408cc22ace2399db5460a45d5943a862
Steve Smith <steve@kentsmiths.com>
2021-05-25 13:30:30 -0400

    Add ReflectionVariance demo

---
+++ src/M3/ReflectionVariance/ReflectionVariance.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReflectionVariance.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
